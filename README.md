6deb9763faac60fce6f57fd9306a1036084f3ea7
gitfilix <runlevel07@gmail.com>
2025-09-30 10:54:40 +0200

    AI agents work as Xpost generator.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ lets try python with openai models.
 
 https://docs.astral.sh/uv/getting-started/installation/#standalone-installer
 
+
 1.1 and install the requests library
 
 ```
@@ -15,5 +16,25 @@ https://docs.astral.sh/uv/getting-started/installation/#standalone-installer
 1.2 dotenv package
 
 ```
-uv add python-dotenv
+% uv add python-dotenv
+```
+
+# run the application locally by
+
+```
+% uv run main.py
+```
+
+
+
+
+Prompt engineering tip:
+
+<em>use XML delimiters</em>
+this helps the LLM- model to understand better the difference between prompts and the input
+
+```
+    <topic>
+    {topic}
+    </topic>
 ```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+# run "uv sync" to install dependencies
 import requests
 from dotenv import load_dotenv
 # load environment variables from .env file
@@ -6,15 +7,20 @@ load_dotenv()
 
 OPEN_API_KEY_DEMO = os.getenv('OPEN_API_KEY_DEMO')
 
-def generate_x_post(user_input: str) -> str:
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+    You are an expert social media manager. Generate a concise and engaging X (formerly Twitter) posts. 
+    Your Task is to generate a post that is concise, engaging, and relevant to the topic provided by the user.
+    Avoid using hashtags, mentions, or links. The post should be no longer than 280 characters.
+    Use Emojis is okay to enhance engagement. Keep the post positive and professional. Keep the post short and focused. use line breaks and empty lines to make the post more readable.
+    Here is the topic provided by the user to generate the post:
+    <topic>
+    {topic}
+    </topic>
+    """
     payload = {
         "model": "gpt-4o",
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant that generates concise and engaging social media posts."},
-            {"role": "user", "content": f"Create a concise and engaging X post about: {user_input}"}
-        ],
-        "max_tokens": 50,
-        "temperature": 0.7
+        "input": prompt
     }
     # call to AI LLM to generate X post
     response = requests.post(
@@ -25,13 +31,18 @@ def generate_x_post(user_input: str) -> str:
             "Authorization": f"Bearer {OPEN_API_KEY_DEMO}"
             }
     )
+    print(f"Response status code: {response.status_code}")
+    # print(f"Response content: {response.content}")
+    response_text = response.json().get('output', [{}])[0].get("content", [{}])[0].get("text", "")
+    return response_text.strip()
 
 
 def main():
-    # user input -> AI LLM to generate X post -> output post
+    # user input -> AI LLM to generate X post text -> output post to be shared on X
     usr_input = input('what should the post be about? ')
     x_post = generate_x_post(usr_input)
-    print(f"Generated X post: {x_post}")
+    print(f"Generated X post: \n")
+    print(x_post)
 
 if __name__ == "__main__":
     main()
